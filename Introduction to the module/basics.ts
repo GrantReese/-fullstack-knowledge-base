5c915b09dd93e0d868c6d5caf1417dd57829a98e
IvaH1992 <119977613+GrantReese@users.noreply.github.com>
2023-09-24 13:08:22 -0500

    First few lessons of typescript

---
+++ Introduction to the module/basics.ts
@@ -0,0 +1,44 @@
+// let age: number;
+
+// age = 12
+
+// let userName: string;
+
+// userName= "Max";
+
+// let isInstructor: boolean;
+
+// isInstructor = true;
+
+// let hobbies: null;
+
+// hobbies = "steve";
+
+let hobbies: string[];
+
+hobbies = ['cooking', 'sports'];
+
+type Person = {
+    name: string;
+    age: number;
+};
+
+let person: Person
+
+person = {
+    name: 'Max',
+    age: 27
+}
+
+// person = {
+//     isEmployee: true
+// };
+
+let people: Person[];
+
+let course: string | number ='React - The Complete Guide';
+
+course = 1234;
+
+
+
